f796729e7769f2954635a77e11f10cb7c2b17d1d
arthi35 <arthimba35@gmail.com>
2022-08-18 20:04:41 +0530

    cors

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ require('./connection')
 
 
 const server = require('http').createServer(app);
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const io = require('socket.io')(server,{
     cors:{
-        origin: '*',
+        origin: 'http://localhost:3000',
         methods: ['GET', 'POST']
     }
 })
