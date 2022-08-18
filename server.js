978aba88d9b896d63b036de40f61deda3029e720
arthi35 <arthimba35@gmail.com>
2022-08-18 19:47:49 +0530

    port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require('./connection')
 
 
 const server = require('http').createServer(app);
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const io = require('socket.io')(server,{
     cors:{
         origin: '*',
