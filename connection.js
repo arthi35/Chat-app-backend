3851249ae46570454b3e95fbbe2ff89e94291b07
arthi35 <arthimba35@gmail.com>
2022-08-20 20:42:30 +0530

    cors

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.oo5m38b.mongodb.net/chatApp?retryWrites=true&w=majority`,()=>{
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@chatapp.1pk6g.mongodb.net/chatapp?retryWrites=true&w=majority`,()=>{
     console.log("DB connected")
 })
